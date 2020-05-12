008416bcca649258a9e5283b35c633ec51647c52
benjnp <benjnp@gmail.com>
2020-05-13 00:26:25 +0800

    Updates

---
+++ precache-manifest.006c9916556bad950cc0ca17982ca0ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310177c62760e8d0edc7adc333d908bd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3b26ca53e3eb50b50b7f",
+    "url": "/monsters-rolodex/static/css/main.8d2ed9dd.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b26ca53e3eb50b50b7f",
+    "url": "/monsters-rolodex/static/js/main.ad26c445.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
